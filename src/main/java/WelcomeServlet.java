9aedc30dd2b102b1a6be9873fa4bb665cda4fb9d
Jared Orler <orlerdev@icloud.com>
2023-05-08 11:13:35 -0500

    commit before exercises

---
+++ src/main/java/WelcomeServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String username = req.getParameter("username");
+    resp.setContentType("text/html");
+    PrintWriter out = resp.getWriter();
+    out.println("<h2>Welcome, " + username + "!<h2>");
+  }
+}
